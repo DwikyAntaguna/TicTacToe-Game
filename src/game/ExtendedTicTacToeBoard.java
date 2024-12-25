fda4b6bc8d8a564a01b30802ace7eea616af5847
Krishna Pramana <Krisnapramana17@gmail.com>
2024-12-25 12:34:03 +0800

    update class Extanded TicTacToeBoard

--- src/game/ExtendedTicTacToeBoard.java
+++ src/game/ExtendedTicTacToeBoard.java
@@ -4,6 +4,12 @@ public class ExtendedTicTacToeBoard extends TicTacToeBoard {
 
     public ExtendedTicTacToeBoard(int size) {
         super(size);
-    }___
+    }
+
+    @Override
+    public boolean checkWin(char symbol) {
+        // Dalam versi ini, kita dapat menambahkan logika tambahan untuk kemenangan jika diperlukan.
+        return super.checkWin(symbol);
+    }
 
 }
